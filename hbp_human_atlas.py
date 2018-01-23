e033cfa92709a9d804197630d32fdccc69b3d6da
Haimasree Bhattacharya <h.bhattacharya@fz-juelich.de>
2018-01-23 15:01:38 +0100

    Update hbp_human_atlas.py

--- hbp_human_atlas.py
+++ hbp_human_atlas.py
@@ -8,10 +8,10 @@ import os
 
 dictionaryimages = {}
 
-#dictionaryimages['FP1'] = 'https://hbp-unic.fz-juelich.de:7112/UFTP/rest/access/JUDAC/2f054eee-7fa5-4ed3-b046-2ddc1315fe9c/ba10m_l_N10_nlin2Stdicbm152casym.nii.gz'
-#dictionaryimages['FP2'] = 'https://hbp-unic.fz-juelich.de:7112/UFTP/rest/access/JUDAC/2f054eee-7fa5-4ed3-b046-2ddc1315fe9c/ba10p_l_N10_nlin2Stdicbm152casym.nii.gz'
-dictionaryimages['FP1'] = 'http://neuroglancer.humanbrainproject.org/precomputed/JuBrain/v2.2c/PMaps/FrontalPole_Fp1.nii'
-dictionaryimages['FP2'] = 'http://neuroglancer.humanbrainproject.org/precomputed/JuBrain/v2.2c/PMaps/FrontalPole_Fp2.nii'
+dictionaryimages['FP1'] = 'https://hbp-unic.fz-juelich.de:7112/UFTP/rest/access/JUDAC/2f054eee-7fa5-4ed3-b046-2ddc1315fe9c/ba10m_l_N10_nlin2Stdicbm152casym.nii.gz'
+dictionaryimages['FP2'] = 'https://hbp-unic.fz-juelich.de:7112/UFTP/rest/access/JUDAC/2f054eee-7fa5-4ed3-b046-2ddc1315fe9c/ba10p_l_N10_nlin2Stdicbm152casym.nii.gz'
+#dictionaryimages['FP1'] = 'http://neuroglancer.humanbrainproject.org/precomputed/JuBrain/v2.2c/PMaps/FrontalPole_Fp1.nii'
+#dictionaryimages['FP2'] = 'http://neuroglancer.humanbrainproject.org/precomputed/JuBrain/v2.2c/PMaps/FrontalPole_Fp2.nii'
 MNI152 = True
 
 class jubrain:        

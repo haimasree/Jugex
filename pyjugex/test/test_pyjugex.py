fb7a6337844a4a2ef4b1a89110db3004a7ad740d
haimasree <hbhattacharya@fz-juelich.de>
2018-02-26 14:09:53 +0100

    added test cases

---
+++ pyjugex/test/test_pyjugex.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from pyjugex import pyjugex
+from pyjugex import hbp_human_atlas as atlas
+
+genelist = ['ADRA2A', 'AVPR1B', 'CHRM2', 'CNR1', 'CREB1', 'CRH', 'CRHR1', 'CRHR2', 'GAD2', 'HTR1A', 'HTR1B', 'HTR1D', 'HTR2A', 'HTR3A', 'HTR5A', 'MAOA', 'PDE1A', 'SLC6A2', 'SLC6A4', 'SST', 'TAC1', 'TPH1', 'GPR50', 'CUX2', 'TPH2']
+#genelist = ['ADRA2A', 'AVPR1B', 'CHRM2']
+roi1 = atlas.jubrain.probability_map('FP1', atlas.MNI152)
+roi2 = atlas.jubrain.probability_map('FP2', atlas.MNI152)
+jugex = pyjugex.Analysis(gene_cache_dir='.pyjugex', verbose=True)
+#jugex = analysispyjugex.Analysis(gene_cache_dir='.pyjugex', single_probe_mode = True, verbose=True)
+result = jugex.DifferentialAnalysis(genelist, roi1, roi2)
+if len([id for id in result if result[id] < .05]) > 0:
+    print('Differentially expressed genes/probes are : ')
+    print([id for id in result if result[id] < .05])
+else:
+    print('There are no differentially expressed genes/probes in the given regions')

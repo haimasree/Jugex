42bb9d5c63c2b4ef84c7799c781f164b72af0a68
Haimasree Bhattacharya <h.bhattacharya@fz-juelich.de>
2018-02-26 14:29:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='pyjugex',
       url='https://github.com/haimasree/Jugex',
       author='Big Data Analytics Group, INM-1, Research Center Juelich',
       author_email='h.bhattacharya@fz-juelich.de',
-      py_modules=['analysispyjugex', 'hbp_human_atlas'],
+      py_modules=['pyjugex', 'hbp_human_atlas'],
       install_requires=['numpy', 'scipy', 'statsmodels', 'requests', 'nibabel', 'xmltodict'], 
       )
